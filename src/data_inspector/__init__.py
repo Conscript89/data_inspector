05c1541272f7071f56ad716085a7af3f67e29caa
Pavel Holica <conscript89@gmail.com>
2024-03-17 21:50:57 +0100

    Move query input to a separate widget

--- src/data_inspector/__init__.py
+++ src/data_inspector/__init__.py
@@ -6,13 +6,15 @@ import json
 import jinja2
 import jinja2.nativetypes
 from textual.app import App
-from textual.validation import Function
-from textual.widgets import Header, Footer, Input, Pretty, Label
+from textual.containers import Vertical
+from textual.widgets import Header, Footer, Pretty, Label
 #import yaml
+
+from .widgets import QueryWidget
 
 
 class DataInspector(App):
-    CSS_PATH = "./layout.tcss"
+    CSS_PATH = "main.tcss"
     BINDINGS = [
         ("ctrl+q", "quit", "Quit")
     ]
@@ -20,42 +22,29 @@ class DataInspector(App):
     def __init__(self, datafile):
         super().__init__()
         self.data = json.load(datafile)
-        self.query_widget = Input(
-            placeholder="Data Query",
-            validate_on=["changed"],
-            validators=Function(self.is_jinja2_expression, "Is not valid jinja2 expression."),
-        )
+        self.query_widget = QueryWidget()
         self.expression_error = Label()
         self.data_widget = Pretty(self.data)
-        self.jinja_env = jinja2.nativetypes.NativeEnvironment()
 
     def compose(self):
         yield Header()
-        yield Label("data")
         yield self.query_widget
         yield self.expression_error
         yield self.data_widget
         yield Footer()
 
     def on_input_changed(self, event):
+        self.expression_error.update(self.query_widget.expression_error)
         if not event.validation_result.is_valid:
             return
-        self.data_widget.update(self.render_expression(event.value, data=self.data))
+        result = self.render_expression(data=self.data)
+        if isinstance(result, jinja2.Undefined):
+            self.expression_error.update('Undefined variable')
+            return
+        self.data_widget.update(result)
 
-    def render_expression(self, expr, **kwargs):
-        jinja_expr = self.jinja_env.compile_expression(f"data{expr}", undefined_to_none=False)
-        return jinja_expr(**kwargs)
-
-    def is_jinja2_expression(self, text):
-        try:
-            self.expression_error.update("")
-            if isinstance(self.render_expression(text, data=self.data), jinja2.runtime.Undefined):
-                self.expression_error.update("Undefined variable")
-                return False
-            return True
-        except jinja2.exceptions.TemplateError as e:
-            self.expression_error.update(e.message)
-            return False
+    def render_expression(self, **kwargs):
+        return self.query_widget.query_template(**kwargs)
 
 
 def cli_parser():
---
+++ src/data_inspector/widgets.py
@@ -0,0 +1,40 @@
+import jinja2.nativetypes
+
+from textual.app import ComposeResult
+from textual.containers import Horizontal
+from textual.validation import Function
+from textual.widgets import Static, Input, Label
+
+
+class QueryWidget(Static):
+    DEFAULT_CSS = """
+    QueryWidget {
+        layout: horizontal;
+    }
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.jinja_env = jinja2.nativetypes.NativeEnvironment()
+        self.query_template = None
+        self.expression_error = ""
+
+    def compose(self) -> ComposeResult:
+        yield Label("data")
+        yield Input(
+            placeholder="Data Query",
+            validate_on=["changed"],
+            validators=Function(self.check_jinja2_expression, "Is not valid jinja2 expression."),
+        )
+
+    def check_jinja2_expression(self, query):
+        try:
+            self.expression_error = ""
+            self.set_query_template(query)
+            return True
+        except jinja2.exceptions.TemplateError as e:
+            self.expression_error = e.message
+            return False
+
+    def set_query_template(self, expression):
+        self.query_template = self.jinja_env.compile_expression(f"data{expression}", undefined_to_none=False)
